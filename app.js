71fbb4ccc52ad1d289217b06eb8836e757404b00
anakavtaradze1 <anakavtaradze111@gmail.com>
2025-06-16 16:32:14 +0400

    add index.html and app.js with variables and primitive types

---
+++ app.js
@@ -0,0 +1,55 @@
+const firstName = "ana";
+console.log(
+  `Variable firstName value is ${firstName}, type is ${typeof firstName}`
+);
+
+const userEmail = "anakavtaradze111@gmail.com";
+console.log(
+  `Variable userEmail value is ${userEmail}, type is ${typeof userEmail}`
+);
+
+const userAge = 22;
+console.log(`Variable userAge value is ${userAge}, type is ${typeof userAge}`);
+
+const isLecturer = false;
+const isStudent = true;
+console.log(
+  `Variable isLecturer value is ${isLecturer}, type is ${typeof isLecturer}`
+);
+console.log(
+  `Variable isStudent value is ${isStudent}, type is ${typeof isStudent}`
+);
+
+let phoneNumber;
+console.log(
+  `Variable phoneNumber value is ${phoneNumber}, type is ${typeof phoneNumber}`
+);
+
+let secondEmail = null;
+console.log(
+  `Variable secondEmail value is ${secondEmail}, type is ${typeof secondEmail}`
+);
+
+const userName = "ana";
+console.log(
+  `Variable userName value is ${userName}, type is ${typeof userName}`
+);
+const dayOfWeek = "saturday";
+console.log(
+  `Variable dayOfWeek value is ${dayOfWeek}, type is ${typeof dayOfWeek}`
+);
+const favoriteActivity = "swimming";
+console.log(
+  `Variable favoriteActivity value is ${favoriteActivity}, type is ${typeof favoriteActivity}`
+);
+
+const greeting = `Hi, my name is ${userName}! Today is ${dayOfWeek}, a perfect day for ${favoriteActivity}`;
+console.log(greeting);
+const greetingMessage =
+  "Hi, my name is " +
+  userName +
+  "! Today is " +
+  dayOfWeek +
+  ", a perfect day for " +
+  favoriteActivity;
+console.log(greetingMessage);
